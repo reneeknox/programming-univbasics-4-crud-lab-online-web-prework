e90dcb41f8e30be55c1bb427d16c1f748a0d5a62
Renee France <renee5.0@outlook.com>
2019-07-13 00:49:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   fruits = ["banana", "peach", "cherry", "apple"]
   fruits << "mango"
-  p
+  p fruits
 end
 
 def add_element_to_start_of_array(array, element)
